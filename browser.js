e35c62ccbef8ca17d1f75e7a81e67c9bf487c1f2
Jovan <mariothrowsfieball@gmail.com>
2022-07-31 12:30:03 +0200

    able to fetch api

--- browser.js
+++ browser.js
@@ -2,11 +2,24 @@ import Signer from "tiktok-signature"; // Import package
 
 const signer = new Signer(); // Create new signer
 await signer.init(); // Create page with. Returns promise
+let url = process.argv.slice(2)[0];
 
-const signature = await signer.sign("tiktok url"); // Get sign for your url. Returns promise
+const signature = await signer.sign(url); // Get sign for your url. Returns promise
 const navigator = await signer.navigator(); // Retrieve navigator data used when signature was generated
 
 console.log(signature);
 console.log(navigator);
+
+let res = await fetch(url, {
+	method: "GET",
+	headers: {
+		"x-tt-params": signature.x_tt_params,
+		"accept-encoding": "gzip, deflate",
+		"User-Agent": navigator.user_agent,
+	},
+});
+
+let data = await res.json();
+console.log(data);
 
 await signer.close(); // Close browser. Returns promise

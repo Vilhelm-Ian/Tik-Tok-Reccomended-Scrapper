c7f1f62112d40a46fa1b84fa5e9aabc3b12b170d
Jovan <mariothrowsfieball@gmail.com>
2022-07-31 13:29:52 +0200

    random useragent on every signature, also added error handling on get_data()

--- browser.js
+++ browser.js
@@ -1,27 +1,37 @@
 import Signer from "tiktok-signature"; // Import package
+import randomUseragent from "random-useragent";
 
-const signer = new Signer(); // Create new signer
-await signer.init(); // Create page with. Returns promise
 let url =
 	"https://m.tiktok.com/api/item_list/?count=30&id=1&type=5&secUid=&maxCursor=0&minCursor=0&sourceType=12&appId=1233&region=CA&language=en";
 
-const signature = await signer.sign(url); // Get sign for your url. Returns promise
-const navigator = await signer.navigator(); // Retrieve navigator data used when signature was generated
+async function generate_signer() {
+	const signer = new Signer(); // Create new signer
+	signer.userAgent = randomUseragent.getRandom();
+	await signer.init(); // Create page with. Returns promise
 
-async function get_data() {
-	let res = await fetch(url, {
-		method: "GET",
-		headers: {
-			"x-tt-params": signature.x_tt_params,
-			"accept-encoding": "gzip, deflate",
-			"User-Agent": navigator.user_agent,
-		},
-	});
-
-	let data = await res.json();
-	console.log(data);
-
+	const signature = await signer.sign(url); // Get sign for your url. Returns promise
+	const navigator = await signer.navigator(); // Retrieve navigator data used when signature was generated
 	await signer.close(); // Close browser. Returns promise
+	return [signature, navigator];
 }
 
-setInterval(() => get_data(), 5000);
+async function get_data(signer) {
+	try {
+		let [signature, navigator] = await signer;
+		let res = await fetch(url, {
+			method: "GET",
+			headers: {
+				"x-tt-params": signature.x_tt_params,
+				"accept-encoding": "gzip, deflate",
+				"User-Agent": navigator.user_agent,
+			},
+		});
+
+		let data = await res.json();
+		console.log(data);
+	} catch (err) {
+		console.error(`failed to get data ${error}`);
+	}
+}
+
+setInterval(() => get_data(generate_signer()), 10000);
